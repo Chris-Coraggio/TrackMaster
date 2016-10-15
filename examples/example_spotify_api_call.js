1b093f3aaa5bef7228aa8893fffe99c1940e11c4
chris-coraggio <ccoraggi@purdue.edu>
2016-10-15 11:14:15 -0400

    Added examples folder

---
+++ examples/example_spotify_api_call.js
@@ -0,0 +1,10 @@
+const api_token = "BQDN-2O72sTUuuBOSBCaQCABs2_bsHTZggHmyUspSGppq6in1qgeZx_HSTPw_diN_T6HhKqgaYhymtWRdgD4kTZjH3VLFI0uE5VtCJp3tOqndYrWRgGz5SjPfiOjtz7geJTpSCExBwaxIYOK";
+
+function getAudioFeatures(spotify_song_id){
+	$.get({
+		url: "https://api.spotify.com/v1/audio"
+	})
+}
+
+
+https://api.spotify.com/v1/audio-features/6rqhFgbbKwnb9MLmUQDhG6?access_token=BQDN-2O72sTUuuBOSBCaQCABs2_bsHTZggHmyUspSGppq6in1qgeZx_HSTPw_diN_T6HhKqgaYhymtWRdgD4kTZjH3VLFI0uE5VtCJp3tOqndYrWRgGz5SjPfiOjtz7geJTpSCExBwaxIYOK
