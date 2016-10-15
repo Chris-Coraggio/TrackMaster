9887cd1755a5855adc25b4843dcf59b8c767361c
Andrew Bass <andybass009@gmail.com>
2016-10-15 13:42:00 -0400

    Get spotify authentication working

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
 var app = require('express')();
+
 var http =require('http').Server(app);
 var io = require('socket.io')(http);
 
+var passwordHash = require('password-hash');
+var passwordGen = passwordHash.generate;
+
 var MongoClient = require('mongodb').MongoClient;
 var dbUrl = 'mongodb://localhost:27017/TrackMaster';
+
+var SpotifyWebApi = require('spotify-web-api-node');
+var spotifyId = 'a385221958aa41d28ece2005e33a9b20';
+var spotifySecret = '033ff04f6e0b4157a67c1870be6f8945';
+
+var spotify = new SpotifyWebApi({
+    clientId: spotifyId,
+    clientSecret: spotifySecret,
+    //redirectUri: "http://trackmaster.me/playlists",
+    redirectUri: "http://localhost:81/playlists",
+});
 
 MongoClient.connect(dbUrl, function(err, db) {
     if (err) {
@@ -16,9 +34,43 @@ MongoClient.connect(dbUrl, function(err, db) {
         console.log("user connected");
     });
 
-    app.get('/', function(req,res){
-        res.sendFile(__dirname + '/index.html');
+    // Set up session
+    app.use(session({
+        secret: 'TrackMasterIsTheBombFuckItShipItImNotGonnaMakeARandomString',
+        cookie: {
+            maxAge: 60000
+        }
+    }));
+
+    // Set up body parser
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    // Basic pages
+    app.get('/', function(req, res){
+        var scopes = [
+            'playlist-read-private',
+            'playlist-read-collaborative',
+            'playlist-modify-public',
+            'playlist-modify-private',
+        ];
+
+        var state = "I'm not sure what to put here";
+        res.redirect(spotify.createAuthorizeURL(scopes, state));
     });
+
+    app.get('/playlists', function(req, res) {
+        spotify.authorizationCodeGrant(req.query.code).then(function(data) {
+            spotify.setAccessToken(data.body.access_token);
+            spotify.setRefreshToken(data.body.refresh_token);
+
+            spotify.getMe().then(function(data) {
+                console.log(data);
+            });
+        });
+    });
+
     app.use(express.static('Public'));
 
     http.listen(process.env.PORT || 81, function(){
