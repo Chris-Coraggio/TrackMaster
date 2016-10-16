fd126d7b5c337a464fd30f18d333dd4e364bcfed
JoeyVinyard <jviny98@yahoo.com>
2016-10-15 21:58:19 -0400

    Added socket.io support to the html

--- Public/js/servercomm.js
+++ Public/js/servercomm.js
@@ -1,3 +1,22 @@
 var server = io();
 //Add socket.io stuff here
-console.log(server);
+var dataObj = {}
+$(document).ready(function(){
+	$("#create-btn").click(function(){
+		dataObj.lyrics = $("#lyrics").val();
+		dataObj.instruments = $("#instr-dropdown").val();
+		var sonLen = parseInt($("#tempo").val());
+		dataObj.songLength =(parseInt($("#minutes_song").val())*60)+parseInt($("#seconds_song").val());
+		dataObj.key = $("#key-dropdown").val();
+		dataObj.dance = parseInt($("#danceAmout").val());
+		//affinities
+		dataObj.lyr = parseInt($("#lyricAffin").val());
+		dataObj.ins = parseInt($("#instAffin").val());
+		dataObj.tem = parseInt($("#tempoAffin").val());
+		dataObj.len = parseInt($("#lengthAffin").val());
+		dataObj.kyA = parseInt($("#keyAffin").val());
+		dataObj.dan = parseInt($("#danceAffin").val());
+		server.emit('songinfo',dataObj);
+	});
+	
+});
